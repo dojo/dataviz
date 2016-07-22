1b69025cf48fd38f64ded78d15f957b9102cd5e0
Mark Wubben <mark@novemberborn.net>
2016-07-22 15:14:39 +0100

    Add provinces to play count data

--- examples/play-counts.ts
+++ examples/play-counts.ts
@@ -6,20 +6,22 @@ import sort from 'src/data/sort';
 export interface PlayCount {
 	artist: string;
 	count: number;
+	province: 'British Columbia' | 'Manitoba' | 'Nova Scotia' | 'Ontario' | 'Quebec';
 }
 
 export default function getObservable (): Observable<PlayCount[]> {
 	const source = new Observable<PlayCount>((subscriber: Subscriber<PlayCount>) => {
-		const data = [
-			{ artist: 'Hawksley Workman', count: 31910 },
-			{ artist: 'Buck 65', count: 21192 },
-			{ artist: 'The Weakerthans', count: 13495 },
-			{ artist: 'Metric', count: 10067 },
-			{ artist: 'The New Pornographers', count: 6201 },
-			{ artist: 'Bike For Three!', count: 6022 },
-			{ artist: 'Mounties', count: 3097 },
-			{ artist: 'Limblifter', count: 2800 },
-			{ artist: 'Arcade Fire', count: 2172 }
+		const data: PlayCount[] = [
+			{ artist: 'Hawksley Workman', count: 31910, province: 'Ontario' },
+			{ artist: 'Buck 65', count: 21192, province: 'Nova Scotia' },
+			{ artist: 'The Weakerthans', count: 13495, province: 'Manitoba' },
+			{ artist: 'Metric', count: 10067, province: 'Ontario' },
+			{ artist: 'The New Pornographers', count: 6201, province: 'British Columbia' },
+			// Crediting it to Buck 65's home province, since Greetings from Tuskan is Belgian.
+			{ artist: 'Bike For Three!', count: 6022, province: 'Nova Scotia' },
+			{ artist: 'Mounties', count: 3097, province: 'Ontario' },
+			{ artist: 'Limblifter', count: 2800, province: 'British Columbia' },
+			{ artist: 'Arcade Fire', count: 2172, province: 'Quebec' }
 		];
 
 		var handle = setInterval(() => { // tslint:disable-line:no-var-keyword
